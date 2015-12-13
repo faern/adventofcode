418c9929fa80b1a90b43e00f4d3226e0117216a8
Linus Färnstrand <faern@faern.net>
2015-12-13 21:58:37 +0100

    Add day 8 part 1

---
+++ advent8/src/main.rs
@@ -0,0 +1,50 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::str::Chars;
+
+fn main() {
+    let data_file = File::open(Path::new("input")).unwrap();
+    let data = BufReader::new(&data_file);
+
+    let mut code_chars = 0;
+    let mut mem_chars = 0;
+    for opt_line in data.lines() {
+        let line = opt_line.unwrap();
+        code_chars += line.len() as u32;
+        mem_chars += count_mem_bytes(&line[..]);
+    }
+    println!("{} code chars minus {} mem chars = {}", code_chars, mem_chars, code_chars - mem_chars);
+}
+
+fn count_mem_bytes(s: &str) -> u32 {
+    assert!(s.len() >= 2);
+    let mut chars = s.chars();
+    while let Some(c) = chars.next() { // Discard starting stuff
+        match c {
+            ' ' => (), // Skip whitespaces
+            '"' => break, // String has started, stop discarding
+            _ => panic!("Malformed string. {} can't appear before opening \"", c),
+        }
+    }
+    let mut mem_bytes = 0;
+    while let Some(c) = chars.next() {
+        match c {
+            '\\' => match chars.next() {
+                Some('\\') => (),
+                Some('"') => (),
+                Some('x') => assert!(read_hex_byte(&mut chars)),
+                _ => panic!("Malformed escape code"),
+            },
+            '"' => break, // String ended
+            _ => (), // Any other char
+        }
+        mem_bytes += 1;
+    }
+    mem_bytes
+}
+
+fn read_hex_byte(iter: &mut Chars) -> bool {
+    let (a, b) = (iter.next().unwrap(), iter.next().unwrap());
+    a.is_digit(16) && b.is_digit(16)
+}
