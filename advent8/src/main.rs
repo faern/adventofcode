c53185f32cff1bbbc203a82b6be2a7384ee85a57
Linus Färnstrand <faern@faern.net>
2015-12-13 22:31:51 +0100

    Add solution for day 8 part 2

--- advent8/src/main.rs
+++ advent8/src/main.rs
@@ -7,18 +7,20 @@ fn main() {
     let data_file = File::open(Path::new("input")).unwrap();
     let data = BufReader::new(&data_file);
 
-    let mut code_chars = 0;
+    let mut input_chars = 0;
     let mut mem_chars = 0;
+    let mut encoded_chars = 0;
     for opt_line in data.lines() {
         let line = opt_line.unwrap();
-        code_chars += line.len() as u32;
+        input_chars += line.len() as u32;
         mem_chars += count_mem_bytes(&line[..]);
+        encoded_chars += encode(&line[..]).len() as u32;
     }
-    println!("{} code chars minus {} mem chars = {}", code_chars, mem_chars, code_chars - mem_chars);
+    println!("{} code chars minus {} mem chars = {}", input_chars, mem_chars, input_chars - mem_chars);
+    println!("Encoding the {} input chars becomes {} encoded chars. An increase of {}", input_chars, encoded_chars, encoded_chars - input_chars);
 }
 
 fn count_mem_bytes(s: &str) -> u32 {
-    assert!(s.len() >= 2);
     let mut chars = s.chars();
     while let Some(c) = chars.next() { // Discard starting stuff
         match c {
@@ -47,4 +49,32 @@ fn count_mem_bytes(s: &str) -> u32 {
 fn read_hex_byte(iter: &mut Chars) -> bool {
     let (a, b) = (iter.next().unwrap(), iter.next().unwrap());
     a.is_digit(16) && b.is_digit(16)
+}
+
+fn encode(s: &str) -> String {
+    let mut chars = s.chars();
+    while let Some(c) = chars.next() { // Discard starting stuff
+        match c {
+            ' ' => (), // Skip whitespaces
+            '"' => break, // String has started, stop discarding
+            _ => panic!("Malformed string. {} can't appear before opening \"", c),
+        }
+    }
+    let mut out_s = String::from("\"\\\"");
+    while let Some(c) = chars.next() {
+        match c {
+            '\\' => {
+                out_s.push_str("\\\\");
+                match chars.next().unwrap() {
+                    '\\' => out_s.push_str("\\\\"),
+                    '"' => out_s.push_str("\\\""),
+                    c2 => out_s.push(c2),
+                }
+            },
+            '"' => break, // String ended
+            _ => out_s.push(c), // Any other char
+        }
+    }
+    out_s.push_str("\\\"\"");
+    out_s
 }
